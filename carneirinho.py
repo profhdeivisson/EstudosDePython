8de826458c1fa9cc5b7e3ad49ae887b95b35e841
Deivisson Henrique <profhdeivisson@yahoo.com>
2019-05-09 08:20:33 -0300

    Delete carneirinho.py

--- carneirinho.py
+++
@@ -1,7 +0,0 @@
-carneirinho = 0
-sono = input('Já dormi?').lower()
-while sono != 'sim':
-        carneirinho += 1
-        print(carneirinho)
-        sono = input('Já dormi?')
-print('Dormiu. FIM!')
