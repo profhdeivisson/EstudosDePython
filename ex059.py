d6103bb3b21d9877fee6a012a55f242ab053df2c
Deivisson Henrique <profhdeivisson@yahoo.com>
2019-05-13 21:42:46 -0300

    first commit

--- ex059.py
+++ ex059.py
@@ -1,28 +1,28 @@
 from time import sleep
 n1 = int(input('Primeiro valor: '))
 n2 = int(input('Segundo valor: '))
-alt = 0
-while alt != 5:
+alternativa = 0
+while alternativa != 5:
     print('=-=' * 10)
-    alt = int(input('''    [1] somar
+    alternativa = int(input('''    [1] somar
     [2] multiplicar
     [3] maior
     [4] novos números
     [5] sair do programa
     >>>>> Qual é a sua opção? 
     '''))
-    if alt == 1:
+    if alternativa == 1:
         print('O resultado da soma entre {} e {} = {}'.format(n1, n2, n1+n2))
-    elif alt == 2:
+    elif alternativa == 2:
         print('O resultado da multiplicação entre {} e {} = {}'.format(n1, n2, n1*n2))
-    elif alt == 3:
+    elif alternativa == 3:
         lista = [n1, n2]
         print('Entre {} e {} o maior número é {}'.format(n1, n2, max(lista)))
-    elif alt == 4:
+    elif alternativa == 4:
         n1 = int(input('Primeiro novo valor: '))
         n2 = int(input('Segundo novo valor: '))
-    elif alt > 5:
-        print('Alternativa inválida! Tente novamente.')
+    elif alternativa > 5:
+        print('alternativaernativa inválida! Tente novamente.')
     else:
         print('Finalizando...')
     sleep(2)
