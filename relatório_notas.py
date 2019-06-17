f6730378eca3612353a0bfc93d310f3865cbee39
Deivisson Henrique <profhdeivisson@yahoo.com>
2019-06-17 18:22:25 -0300

    first commit

--- relatório_notas.py
+++ relatório_notas.py
@@ -1,5 +1,5 @@
 notas = []
-total = acimaMédia = 0
+total = acimaMédia = contMenorSete = 0
 while True:
     nota = 0
     nota = int(input('Digite uma nota: '))
@@ -10,12 +10,15 @@ while True:
 print(f'Há {len(notas)} itens na lista.')
 for c in notas:
     print(f'{c}', end=' ')
+    if c < 7:
+        contMenorSete += 1
+    if c > total / len(notas):
+        acimaMédia += 1
 print('')
 for c in reversed(notas):
     print(c)
 print('Total = {}'.format(total))
-print(f'A média dos valores digitados é: {total / len(notas)}')
-for c in notas:
-    if c > total / len(notas):
-        acimaMédia += 1
+print(f'A média dos valores digitados é: {total / len(notas):.2f}')
 print(f'{acimaMédia} números estão acima da média.')
+print(f'{contMenorSete} números são menores que 7.')
+print('Volte sempre!')
