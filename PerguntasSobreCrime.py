7f9627da57a8857262f874db01d241c2e084827c
Deivisson Henrique <profhdeivisson@yahoo.com>
2019-06-19 18:39:20 -0300

    Alterações recomendadas pelo Raul

--- PerguntasSobreCrime.py
+++ PerguntasSobreCrime.py
@@ -1,19 +1,10 @@
-questions = ['Telefonou para a vítima?', 'Esteve no local do crime?', 'Mora perto da vítima?', 'Devia para a vítima?', 'Já trabalhou com a vítima?']
+questions = {1: 'Telefonou para a vítima?', 2: 'Esteve no local do crime?', 3: 'Mora perto da vítima?', 4: 'Devia para a vítima?', 5: 'Já trabalhou com a vítima?'}
+análise = {0: 'Inocente', 1: 'Inocente', 2: 'Suspeito', 3: 'Cúmplice', 4: 'Cúmplice', 5: 'Assassino'}
 respostas = []
-contRespostas = 0
-
-for q in questions:
+for q in questions.values():
     x = ' '
     while x not in 'sn':
         x = input(q).strip().lower()[0]
-    respostas.append(x)
     if x == 's':
-        contRespostas += 1
-if contRespostas == 2:
-    print('Classificado como SUSPEITO!')
-elif contRespostas == 3 or contRespostas == 4:
-    print('Classificado como CÚMPLICE!')
-elif contRespostas == 5:
-    print('Classificado como ASSASSINO!')
-else:
-    print('Classificado como INOCENTE!')
+        respostas.append(x)
+print(f'Você é {análise[len(respostas)]}')
